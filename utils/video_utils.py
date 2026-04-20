3ebe9c748c45446d497b614eef35827fe3364291
prakhar175 <prakhar.k2004@gmail.com>
2026-04-20 23:00:57 +0530

    added training and utils

---
+++ utils/video_utils.py
@@ -0,0 +1,17 @@
+import cv2
+
+def read_video(video_path):
+    cap = cv2.VideoCapture(video_path)
+    frames = []
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        frames.append(frame)
+    return frames
+def save_video(output_video_frames, output_path, fps=30):
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    out = cv2.c(output_path, fourcc, fps, (output_video_frames[0].shape[1], output_video_frames[0].shape[0]))
+    for frame in output_video_frames:
+        out.write(frame)
+    out.release()
